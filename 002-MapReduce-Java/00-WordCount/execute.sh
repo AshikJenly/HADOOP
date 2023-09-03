695c1d2c1e9d7f17a8b09997f3fa0f84f039d988
ASHIK JENLY <116492348+AshikJenly@users.noreply.github.com>
2023-09-03 14:40:10 +0530

    Add files via upload

--- 002-MapReduce-Java/00-WordCount/execute.sh
+++ 002-MapReduce-Java/00-WordCount/execute.sh
@@ -1,6 +1,27 @@
-clear
-javac WordCount.java
-jar -cvf wordcount.jar *.class
-hadoop jar wordcount.jar WordCount /user/inputdata/inputfile.txt /user/wordcountoutput
+hdfs dfs -mkdir /user
+hdfs dfs -mkdir /user/mapreduce
+hdfs dfs -mkdir /user/mapreduce/ex00
+hdfs dfs -mkdir /user/mapreduce/ex00/input
 
-hdfs dfs -get /user/wordcountoutput/ .
+
+INPUT_FILE_NAME="*"
+
+INPUT_PATH="/user/mapreduce/ex03/input/datas"
+OUTPUT_PATH="/user/mapreduce/ex03/output"
+DATA_SOURCE_PATH="./datas"
+MAIN_JAVA_FILE_NAME="WordCount.java"
+MAIN_CLASS_NAME="WordCount"
+JAR_NAME="Count.jar"
+
+hdfs dfs -put $DATA_SOURCE_PATH  $INPUT_PATH
+rm *.class
+rm *.jar
+javac $MAIN_JAVA_FILE_NAME
+jar -cvf $JAR_NAME *.class
+
+hdfs dfs -rm -r $OUTPUT_PATH
+hadoop jar $JAR_NAME $MAIN_CLASS_NAME $INPUT_PATH/$INPUT_FILE_NAME $OUTPUT_PATH 
+
+rm -r ./output
+hdfs dfs -get $OUTPUT_PATH .
+cat ./output/part-r-00000
