0ee96e49c7c15d06bfa900118ddc7f54601d4a5c
ASHIK JENLY <116492348+AshikJenly@users.noreply.github.com>
2023-09-03 14:39:44 +0530

    Add files via upload

--- 002-MapReduce-Java/01-MaxMin/execute.sh
+++ 002-MapReduce-Java/01-MaxMin/execute.sh
@@ -1,10 +1,26 @@
-clear
+hdfs dfs -mkdir /user
+hdfs dfs -mkdir /user/mapreduce
+hdfs dfs -mkdir /user/mapreduce/ex00
+hdfs dfs -mkdir /user/mapreduce/ex00/input
+
+
+INPUT_FILE_NAME="*.op"
+INPUT_PATH="/user/mapreduce/ex03/input/datas"
+OUTPUT_PATH="/user/mapreduce/ex03/output"
+DATA_SOURCE_PATH="./datas"
+MAIN_JAVA_FILE_NAME="MaxMin.java"
+MAIN_CLASS_NAME="MaxMin"
+JAR_NAME="maxminjar.jar"
+
+hdfs dfs -put $DATA_SOURCE_PATH/$INPUT_FILE_NAME  $INPUT_PATH
 rm *.class
-javac MaxMin.java 
 rm *.jar
-jar -cvf maxmin.jar *.class
+javac $MAIN_JAVA_FILE_NAME
+jar -cvf $JAR_NAME *.class
 
-hdfs dfs -rm -r /user/MaxMinOutput
-hadoop jar maxmin.jar MaxMin /user/MaxMinData/* /user/MaxMinOutput
+hdfs dfs -rm -r $OUTPUT_PATH
+hadoop jar $JAR_NAME $MAIN_CLASS_NAME $INPUT_PATH/$INPUT_FILE_NAME $OUTPUT_PATH 
 
-hdfs dfs -get /user/MaxMinOutput/* .
+rm -r ./output
+hdfs dfs -get $OUTPUT_PATH .
+cat ./output/part-r-00000
