0b9315af6be4cdc0905c7867d00b7c26f01bd8cd
ASHIK JENLY <116492348+AshikJenly@users.noreply.github.com>
2023-09-03 14:39:03 +0530

    Add files via upload

--- 002-MapReduce-Java/02-SimpleAggregrateMetric/execute.sh
+++ 002-MapReduce-Java/02-SimpleAggregrateMetric/execute.sh
@@ -1,12 +1,26 @@
-clear
-hdfs dfs -put ./datas/NASA_access_log_Jul95 /user/mapreduce/ex02/input/
-rm ./*.class
-javac LogAnalysis.java 
-rm ./*.jar
-jar -cvf LogJar.jar *.class
+hdfs dfs -mkdir /user
+hdfs dfs -mkdir /user/mapreduce
+hdfs dfs -mkdir /user/mapreduce/ex00
+hdfs dfs -mkdir /user/mapreduce/ex00/input
 
-hdfs dfs -rm -r /user/mapreduce/ex02/output
-hadoop jar LogJar.jar LogAnalysis /user/mapreduce/ex02/input/NASA_access_log_Jul95 /user/mapreduce/ex02/output
+
+INPUT_FILE_NAME="NASA_access_log_Jul95"
+INPUT_PATH="/user/mapreduce/ex03/input/datas"
+OUTPUT_PATH="/user/mapreduce/ex03/output"
+DATA_SOURCE_PATH="./datas"
+MAIN_JAVA_FILE_NAME="LogAnalysis.java"
+MAIN_CLASS_NAME="LogAnalysis"
+JAR_NAME="logjar.jar"
+
+hdfs dfs -put $DATA_SOURCE_PATH/$INPUT_FILE_NAME  $INPUT_PATH
+rm *.class
+rm *.jar
+javac $MAIN_JAVA_FILE_NAME
+jar -cvf $JAR_NAME *.class
+
+hdfs dfs -rm -r $OUTPUT_PATH
+hadoop jar $JAR_NAME $MAIN_CLASS_NAME $INPUT_PATH/$INPUT_FILE_NAME $OUTPUT_PATH 
+
 rm -r ./output
-hdfs dfs -get /user/mapreduce/ex02/output .
+hdfs dfs -get $OUTPUT_PATH .
 cat ./output/part-r-00000
